3fba26280598c408806bae3315713e4e4cdf20c7
learner <huzefaratlam63@gmail.com>
2025-02-27 22:31:45 +0530

    Update Button.jsx

--- app/_components/Button.jsx
+++ app/_components/Button.jsx
@@ -1,9 +1,9 @@
-import { login } from "@/actions/chatActions";
+ import { login } from "@/actions/chatActions";
 
 function Button() {
     return (
       <form action={login}>
-        <button className="bg-black rounded-lg px-4 py-2 text-white hover:cursor-pointer hover:text-gray-100 hover:-translate-y-[10px]">
+        <button className="bg-black rounded-lg px-4 py-2 text-white hover:cursor-pointer hover:text-gray-100 hover:-translate-y-[10px] transition-all duration-[0.3s]">
           Login
         </button>
       </form>
