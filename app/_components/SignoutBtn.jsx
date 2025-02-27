7f574a36275db920cee3b5866fe2c3e9b0a58660
learner <huzefaratlam63@gmail.com>
2025-02-27 22:23:26 +0530

    Update SignoutBtn.jsx

--- app/_components/SignoutBtn.jsx
+++ app/_components/SignoutBtn.jsx
@@ -3,7 +3,7 @@ import { logout } from "@/actions/chatActions";
 function Button() {
     return (
       <form action={logout}>
-        <button className="bg-black rounded-lg px-4 py-2 text-white">
+        <button className="bg-black rounded-lg ml-5 px-4 py-2 text-white">
           Logout
         </button>
       </form>
