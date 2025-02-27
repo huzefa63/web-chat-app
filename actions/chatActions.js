4715933981a698a4c36d4021f62085efa9312322
learner <huzefaratlam63@gmail.com>
2025-02-27 22:19:32 +0530

    Update chatActions.js

--- actions/chatActions.js
+++ actions/chatActions.js
@@ -1,6 +1,9 @@
 "use server";
-import { signIn } from "@/lib/googleAuth";
+import { signIn,signOut } from "@/lib/googleAuth";
 
 export async function login(){
     await signIn('google',{redirectTo:'/'});
+}
+export async function logout(){
+    await signOut({redirectTo:'/'});
 }
