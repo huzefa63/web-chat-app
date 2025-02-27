c59fda46e1bc9ee639188afa7654fda93c56ead8
unknown <huzefaratlam63@gmail.com>
2025-02-27 21:44:57 +0530

    Resolved merge conflict in NextAuth config

--- actions/chatActions.js
+++ actions/chatActions.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 "use server";
 import { signIn } from "@/lib/googleAuth";
 
 export async function login(){
     await signIn('google',{redirectTo:'/'});
-=======
-"use server";
-import { signIn } from "@/lib/googleAuth";
-
-export async function login(){
-    await signIn('google',{redirectTo:'/'});
->>>>>>> 6ae2ef5 (Your commit message)
 }
