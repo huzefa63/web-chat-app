b6ccf41b57564bbf59b5677083443e872d2b8a4c
learner <huzefaratlam63@gmail.com>
2025-02-28 05:50:55 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import { auth } from "@/lib/googleAuth";
 export default async function Home() {
   const session = await auth();
   return (
-    <div className="flex h-screen lg:items-center mt-[2rem] justify-center">
+    <div className="flex h-screen lg:items-center mt-[9rem] justify-center">
       <Chatbox session={session} />
     </div>
   );
